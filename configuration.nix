6eb69427c5bf652583137c4f00ee490e43514fa7
Pim Snel <post@pimsnel.com>
2024-10-31 13:47:07 +0000

    stuff

--- configuration.nix
+++ configuration.nix
@@ -8,23 +8,56 @@
   services.openssh.enable = true;
   services.tailscale.enable = true;
 
+  users.users.pim = {
+	  name = "pim";
+	  home = "/home/pim";
+	  isNormalUser = true;
+	  shell = "/run/current-system/sw/bin/zsh";
+	  group = "wheel";
+	  extraGroups = ["nixusers"];
+  };
+
+
   nix = {
     extraOptions = ''experimental-features = nix-command flakes'';
+    package = pkgs.nixFlakes;
+
+    settings.trusted-substituters = [
+      "http://attic.tools.technative.cloud:8080/smartmc"
+    ];
+    settings.extra-substituters = [
+      "http://attic.tools.technative.cloud:8080/smartmc"
+    ];
+    settings.extra-trusted-public-keys = [
+      "smartmc:uix5eOnUkmqH6VzKToVNFyEr7PNUmXKt1QDQqVdv2XA="
+    ];
+
   };
+
   
   environment.systemPackages = with pkgs; [
+    #luxery
+    zsh
     vim
+    home-manager
+    age
     tmux
-    xdg-utils
-    inkscape
-    curl
     rsync
+
+    #extra
+    xdg-utils
+    neofetch
+    dialog
+
+    #basic devops
+    tfswitch
+    curl
+    attic-client
+    aws-mfa
     jq
     awscli2
     gum
     granted
-    neofetch
-    dialog
     git
   ];
 
